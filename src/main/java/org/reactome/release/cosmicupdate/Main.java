8b9b4992b1b1e15126d680aed44f12e051a69c00
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2020-11-10 15:07:00 -0500

    Use COSMICFileRetriever

--- src/main/java/org/reactome/release/cosmicupdate/Main.java
+++ src/main/java/org/reactome/release/cosmicupdate/Main.java
@@ -4,6 +4,8 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.SQLException;
 import java.time.Duration;
 import java.util.Collection;
@@ -21,6 +23,7 @@ import org.gk.model.ReactomeJavaConstants;
 import org.gk.persistence.MySQLAdaptor;
 import org.gk.schema.InvalidAttributeException;
 import org.reactome.release.common.ReleaseStep;
+import org.reactome.release.common.dataretrieval.cosmic.COSMICFileRetriever;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
@@ -39,6 +42,9 @@ public class Main extends ReleaseStep
 	private static String COSMICFusionExport;
 	private static String COSMICMutationTracking;
 
+	private static String COSMICUsername;
+	private static String COSMICPassword;
+	
 	private static final Logger logger = LogManager.getLogger();
 	private static long creatorID;
 	public static void main(String... args)
@@ -52,6 +58,8 @@ public class Main extends ReleaseStep
 				Main.COSMICMutantExport = props.getProperty("pathToMutantExportFile", "./CosmicMutantExport.tsv");
 				Main.COSMICFusionExport = props.getProperty("pathToFusionExportFile", "./CosmicFusionExport.tsv");
 				Main.COSMICMutationTracking = props.getProperty("pathToMutationTrackingFile", "./CosmicMutationTracking.tsv");
+				Main.COSMICUsername = props.getProperty("cosmic.username");
+				Main.COSMICPassword = props.getProperty("cosmic.password");
 				Main cosmicUpdateStep = new Main();
 				JCommander.newBuilder().addObject(cosmicUpdateStep).build().parse(args);
 				cosmicUpdateStep.executeStep(props);
@@ -83,6 +91,9 @@ public class Main extends ReleaseStep
 			// TODO: Add a file downloader to retrieve the COSMIC files.
 			MySQLAdaptor adaptor = ReleaseStep.getMySQLAdaptorFromProperties(props);
 			loadTestModeFromProperties(props);
+			
+			downloadFiles();
+			
 			Collection<GKInstance> cosmicObjects = COSMICUpdateUtil.getCOSMICIdentifiers(adaptor);
 			logger.info("{} COSMIC identifiers", cosmicObjects.size());
 			// Filter the identifiers to exclude the COSV prefixes. 
@@ -112,6 +123,61 @@ public class Main extends ReleaseStep
 				updateIdentifiers(adaptor, updates);
 			}
 		}
+	}
+
+	private void downloadFiles() throws Exception
+	{
+		COSMICFileRetriever mutantExportRetriever = new COSMICFileRetriever();
+		COSMICFileRetriever mutationTrackingRetriever = new COSMICFileRetriever();
+		COSMICFileRetriever fusionExportRetriever = new COSMICFileRetriever();
+		
+		try
+		{
+			mutantExportRetriever.setDataURL(new URI(Main.COSMICMutantExport));
+			mutationTrackingRetriever.setDataURL(new URI(Main.COSMICMutationTracking));
+			fusionExportRetriever.setDataURL(new URI(Main.COSMICFusionExport));
+			
+			mutantExportRetriever.setFetchDestination("./mutant_export.csv");
+			mutationTrackingRetriever.setFetchDestination("./mutation_tracking.csv");
+			fusionExportRetriever.setFetchDestination("./fusion_export.csv");
+			
+//			mutantExportRetriever.setMaxAge(Duration.ofHours(24));
+//			mutantExportRetriever.setUserName(Main.COSMICUsername);
+//			mutantExportRetriever.setPassword(Main.COSMICPassword);
+//			mutantExportRetriever.fetchData();
+			this.executeDownload(mutantExportRetriever);
+			
+//			mutationTrackingRetriever.setMaxAge(Duration.ofHours(24));
+//			mutationTrackingRetriever.setUserName(Main.COSMICUsername);
+//			mutationTrackingRetriever.setPassword(Main.COSMICPassword);
+//			mutationTrackingRetriever.fetchData();
+			this.executeDownload(mutationTrackingRetriever);
+			
+//			fusionExportRetriever.setMaxAge(Duration.ofHours(24));
+//			fusionExportRetriever.setUserName(Main.COSMICUsername);
+//			fusionExportRetriever.setPassword(Main.COSMICPassword);
+//			fusionExportRetriever.fetchData();
+			this.executeDownload(fusionExportRetriever);
+		}
+		catch (URISyntaxException e)
+		{
+			e.printStackTrace();
+			throw e;
+		}
+		catch (Exception e)
+		{
+			logger.error("Error occurred while trying to download a file! " + e.getMessage(), e);
+			// throw the exception further up the stack - can't continue if file downloads are failing.
+			throw e;
+		}
+	}
+	
+	private void executeDownload(COSMICFileRetriever retriever) throws Exception
+	{
+		retriever.setMaxAge(Duration.ofHours(24));
+		retriever.setUserName(Main.COSMICUsername);
+		retriever.setPassword(Main.COSMICPassword);
+		retriever.fetchData();
 	}
 	
 	/**
